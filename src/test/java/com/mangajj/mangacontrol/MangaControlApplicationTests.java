3dfe9dab32fbd77ebdc2a69633e9a53ad917fc03
jjeanjacques10 <jjean.jacques10@gmail.com>
2022-01-04 18:51:55 -0300

    add runner

--- src/test/java/com/mangajj/mangacontrol/MangaControlApplicationTests.java
+++ src/test/java/com/mangajj/mangacontrol/MangaControlApplicationTests.java
@@ -1,13 +1,11 @@
 package com.mangajj.mangacontrol;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MangaControlApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
